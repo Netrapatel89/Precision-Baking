e4fa7aa61d8986ccd67a3efe6118d0e3725a3452
Netrapatel89 <netrapatel8034@gmail.com>
2026-07-06 12:51:31 +0530

    Add files via upload

---
+++ src/app/api/convert-measurements/route.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+import { exec } from "child_process";
+import fs from "fs";
+import { promisify } from "util";
+import jwt from "jsonwebtoken";
+import Conversion from "@/models/Conversion";
+
+const execAsync = promisify(exec);
+
+export async function POST(req: NextRequest) {
+  const { recipe } = await req.json();
+  const token = req.cookies.get("token")?.value || null;
+  if (!token) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  if (!recipe) {
+    return NextResponse.json(
+      { message: "Please provide a recipe to convert" },
+      { status: 400 }
+    );
+  }
+  try {
+    const decodedId = jwt.verify(token, process.env.JWT_SECRET!) as {
+      id: string;
+    };
+    const recipeFilePath = "python/recipe.txt";
+    fs.writeFileSync(recipeFilePath, recipe, "utf8");
+    const { stdout, stderr } = await execAsync(`python python/convert.py`);
+    if (stderr) {
+      console.log(stderr);
+    }
+    const parsedRecipe = JSON.parse(stdout.trim());
+    const saveConversion = new Conversion({
+      user: decodedId.id,
+      input: recipe,
+      output: JSON.stringify(parsedRecipe),
+    });
+    await saveConversion.save();
+    return NextResponse.json({ recipe: parsedRecipe }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Something went wrong!!!" },
+      { status: 500 }
+    );
+  }
+}
